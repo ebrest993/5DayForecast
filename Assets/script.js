3d3ae8209522235752b3517cb29979f259903192
Elliott Brest <elliott.brest@gmail.com>
2023-07-24 16:12:24 -0500

    askbcs push

--- Assets/script.js
+++ Assets/script.js
@@ -6,19 +6,18 @@ let countryCode = '';
 let limit = '';
 
 let testButton = document.querySelector(".button")
-let urlRequest = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=4d8f86000d241e776281dc749be197b9';
+let urlRequest = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=4d8f86000d241e776281dc749be197b9`;
 
 
 console.log();
 
-// ',' + stateCode + ',' + countryCode + '&limit=' + limit + 
 
 // testButton.addEventListener('click', GO);
 
 GO();
 
-function GO () {
-let locationRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=4d8f86000d241e776281dc749be197b9';
+function GO (cityName, stateCode, countryCode, limit) {
+let locationRequest = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=4d8f86000d241e776281dc749be197b9`;
 fetch(locationRequest)
   .then (function (response) {
   if (!response.ok) {
@@ -26,7 +25,7 @@ fetch(locationRequest)
   } else {
     response.json()
   .then (function(data) {
-    console.log(data.items, cityName);
+    console.log(data.items, cityName, stateCode, countryCode, limit);
 })}})}
 
 
