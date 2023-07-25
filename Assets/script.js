7f61fafaaca1ca57eea31229b02943fbbb42658d
Elliott Brest <elliott.brest@gmail.com>
2023-07-25 18:04:21 -0500

    getting responses with fetch

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 let cityName = document.querySelector('#userInput');
 let theButton = document.querySelector(".btn");
-let urlRequest = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=4d8f86000d241e776281dc749be197b9`;
+// let urlRequest = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=4d8f86000d241e776281dc749be197b9`;
 
 console.log();
 
@@ -10,23 +10,30 @@ function GO (event) {
   let locationRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName.value + '&limit=1&appid=4d8f86000d241e776281dc749be197b9';
   fetch(locationRequest)
     .then(function(response) {
-      response.json();
-      console.log(response)
-      // document.location.replace('./search-results.html')
-    }
-  );
-  event.preventDefault()
-};
-    
-    
-    // if(response) {
-//   alert("please enter a valid city name");
-// } else {
+      return response.json();
+    })
+    .then(function (BecauseItHasToMatchToFinishTheExpression) {
+      console.log(BecauseItHasToMatchToFinishTheExpression)
+    })
+    event.preventDefault();
+  };
+
   
-  //     .then (function (response) {
-  //     if (!response.ok) {
-  //     console.log('for the love of god');
-  //   } else {
+
+
+
+
+
+  // document.location.replace('./search-results.html')
+    
+  // if(response) {
+    //   alert("please enter a valid city name");
+    // } else {
+      
+      //     .then (function (response) {
+        //     if (!response.ok) {
+          //     console.log('for the love of god');
+          //   } else {
   //     response.json()
   //       .then (function() {
   //         currentTarget.preventDefault()
