c947e6e622b9e3868f2908fef53db7afddcfdff1
Elliott Brest <elliott.brest@gmail.com>
2023-07-25 19:23:09 -0500

    created for loop to give 5 days worth of information

--- Assets/script.js
+++ Assets/script.js
@@ -14,16 +14,18 @@ const locationRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city
     .then(function (newResponse) {
       const latitude = newResponse[0].lat;
       const longitude = newResponse[0].lon;
-      const urlRequest = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude.value + '&lon=' + longitude.value + '&appid=4d8f86000d241e776281dc749be197b9';
+      const urlRequest = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=4d8f86000d241e776281dc749be197b9';
       console.log(latitude)
       console.log(longitude)
       fetch(urlRequest)
         .then(function(response) {
           return response.json()
-          .then(function (newResponseAgain) {
-            console.log(newResponseAgain)
-    });
-      })
-      })
+        })
+        .then(function (secRes) {
+          for (i=0; i<=4; i++) {
+          console.log(secRes.list[i].dt_txt)
+      }
+    })
+  })
     event.preventDefault();
-  };
+};
