828d8677b323d2124cc514e53f95c3dd34b20a8d
Elliott Brest <elliott.brest@gmail.com>
2023-07-26 14:57:40 -0500

    weather data displayed dynamically

--- Assets/script.js
+++ Assets/script.js
@@ -10,70 +10,89 @@ let test = document.querySelector(".test");
 
 theButton.addEventListener('click', GO);
 
-function GO (event) {
-  const locationRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName.value + '&limit=1&appid=4d8f86000d241e776281dc749be197b9';
-  historyButtons (cityName);
-  console.log();
-  fetchInfo(locationRequest);
-  event.preventDefault();
-}  
-function fetchInfo(locationRequest){
-  fetch(locationRequest)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function (newResponse) {
-      const latitude = newResponse[0].lat;
-      const longitude = newResponse[0].lon;
-      const newCity = newResponse[0].name;
-      const urlRequest = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=4d8f86000d241e776281dc749be197b9&units=imperial';
-      console.log(latitude)
-      console.log(longitude)
-      fetch(urlRequest)
-        .then(function(response) {
-          return response.json()
+function GO(event) {
+    const locationRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName.value + '&limit=1&appid=4d8f86000d241e776281dc749be197b9';
+    historyButtons(cityName);
+    console.log();
+    fetchInfo(locationRequest);
+    event.preventDefault();
+}
+function fetchInfo(locationRequest) {
+    fetch(locationRequest)
+        .then(function (response) {
+            return response.json();
         })
-        .then(function (secRequ) {
-          console.log(secRequ)
-          sharedData (secRequ, newCity);
+        .then(function (newResponse) {
+            const latitude = newResponse[0].lat;
+            const longitude = newResponse[0].lon;
+            const newCity = newResponse[0].name;
+            const urlRequest = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=4d8f86000d241e776281dc749be197b9&units=imperial';
+            console.log(latitude)
+            console.log(longitude)
+            fetch(urlRequest)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (secRequ) {
+                    console.log(secRequ)
+                    sharedData (secRequ, newCity);
+                })
         })
-      })
-    };
+};
+
+function sharedData(secRequ, newCity) {
+    console.log(secRequ, newCity)
     
-    function sharedData (secRequ, newCity) {
-      currentWeather (newCity, secRequ.list[0]);
-      // forecastWeather (secRequ.list);
-      console.log();
-    }
-    
-    function historyButtons(city) {
-      console.log(city);
-    }
-    
-    function currentWeather (newCity, data) {
-      let currentCard = document.createElement("p");
-      // test.append(currentCard);
-      let weatherData = currentCard.textContent =
-      [data.main.temp,
-      data.weather[0].description,
-      ("Feels like: " + data.main.feels_like),
-      ('Low: ' + data.main.temp_min),
-      ('High: ' + data.main.temp_max)];      
-      createCard (currentCard, weatherData);
-      setAttrFunc(currentCard, weatherData);
+    currentWeather(secRequ.list, secRequ.city.name);
+
+    // forecastWeather (secRequ.list);
+    console.log();
 }
 
-function setAttrFunc (currentCard) {
-  currentCard.setAttribute = ("class", "card");
-  currentCard.setAttribute = ("class", "card-body");
-  currentCard.setAttribute = ("class", "card-title");
-  currentCard.setAttribute = ("class", "weather");
-  currentCard.setAttribute = ("class", "feels-like");
+function historyButtons(city) {
+    console.log(city);
 }
 
-function createCard (currentCard, weatherData) {
-  for (i=0; i < weatherData.length; i++) {
-  weatherData = document.createElement("li");
-  }
-  console.log(weatherData.length);
+function currentWeather(data,city) {
+    for (let i = 0; i < data.length; i = i + 8) {
+        let currentCard = document.createElement("div");
+        let currentTitle = document.createElement("h6")
+        let currentTemp = document.createElement("div")
+        let currentDescr= document.createElement("div")
+        let currentFeels = document.createElement("div")
+        let tempLow = document.createElement("div");
+        let tempHigh = document.createElement("div");
+        
+
+        // test.append(currentCard);
+        currentTitle.textContent = city;
+        currentTemp.textContent = "Temp: " + data[i].main.temp
+        currentDescr.textContent = " Description: " + data[i].weather[0].description
+        currentFeels.textContent = "Feels like: " + data[i].main.feels_like
+        tempLow.textContent = 'Low: ' + data[i].main.temp_min
+        tempHigh.textContent = 'High: ' + data[i].main.temp_max
+
+        currentCard.setAttribute = ("class", "card");
+        currentTitle.setAttribute = ("class", "card-title");
+        currentTemp.setAttribute = ("class", "card-body");
+        currentDescr.setAttribute = ("class", "card-body");
+        currentFeels.setAttribute = ("class", "card-body");
+        tempLow.setAttribute = ("class", "card-body");
+        currentCard.setAttribute = ("class", "weather");
+        currentCard.setAttribute = ("class", "feels-like");
+        currentCard.appendChild(currentTitle);
+        currentCard.appendChild(currentTemp);
+        currentCard.appendChild(currentDescr);
+        currentCard.appendChild(tempHigh);
+        currentCard.appendChild(tempLow);
+        document.querySelector(".test").append(currentCard)
+        console.log(data[i])
+    }
+}
+
+function createCard(currentCard, weatherData) {
+    for (i = 0; i < weatherData.length; i++) {
+        weatherData = document.createElement("li");
+    }
+    console.log(weatherData.length);
 }
--- README.md
+++ README.md
@@ -1,4 +1,92 @@
+### ASK BCS SOLVE ###
 
+let cityName = document.querySelector('#userInput');
+let theButton = document.querySelector(".btn");
+let rightNow = document.querySelector(".right-now");
+let weatherCond = document.querySelector(".weather")
+let feelsLike = document.querySelector(".feels-like");
+let highTemp = document.querySelector(".high");
+let lowTemp = document.querySelector(".low");
+let cards = document.querySelectorAll(".card");
+let test = document.querySelector(".test");
+
+theButton.addEventListener('click', GO);
+
+function GO(event) {
+    const locationRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName.value + '&limit=1&appid=4d8f86000d241e776281dc749be197b9';
+    historyButtons(cityName);
+    console.log();
+    fetchInfo(locationRequest);
+    event.preventDefault();
+}
+function fetchInfo(locationRequest) {
+    fetch(locationRequest)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (newResponse) {
+            const latitude = newResponse[0].lat;
+            const longitude = newResponse[0].lon;
+            const newCity = newResponse[0].name;
+            const urlRequest = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=4d8f86000d241e776281dc749be197b9&units=imperial';
+            console.log(latitude)
+            console.log(longitude)
+            fetch(urlRequest)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (secRequ) {
+                    console.log(secRequ)
+                    sharedData (secRequ, newCity);
+                })
+        })
+};
+
+function sharedData(secRequ, newCity) {
+    console.log(secRequ, newCity)
+    
+    currentWeather(secRequ.list, secRequ.city.name);
+
+    // forecastWeather (secRequ.list);
+    console.log();
+}
+
+function historyButtons(city) {
+    console.log(city);
+}
+
+function currentWeather(data,city) {
+    for (let i = 0; i < data.length; i = i + 8) {
+        let currentCard = document.createElement("div");
+        let currentTitle = document.createElement("h6")
+        let currentBody = document.createElement("div")
+        // test.append(currentCard);
+        currentTitle.textContent = city
+        currentBody.textContent = "Temp :"+
+            data[i].main.temp + "Descirption"+
+            data[i].weather[0].description + 
+            ("Feels like: " + data[i].main.feels_like)+
+            ('Low: ' + data[i].main.temp_min)+
+            ('High: ' + data[i].main.temp_max);
+
+        currentCard.setAttribute = ("class", "card");
+        currentBody.setAttribute = ("class", "card-body");
+        currentTitle.setAttribute = ("class", "card-title");
+        currentCard.setAttribute = ("class", "weather");
+        currentCard.setAttribute = ("class", "feels-like");
+        currentCard.appendChild(currentTitle)
+        currentCard.appendChild(currentBody)
+        document.querySelector(".test").append(currentCard)
+        console.log(data[i])
+    }
+}
+
+function createCard(currentCard, weatherData) {
+    for (i = 0; i < weatherData.length; i++) {
+        weatherData = document.createElement("li");
+    }
+    console.log(weatherData.length);
+}
 
 
 
