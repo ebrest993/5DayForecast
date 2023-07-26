4ffa1e3313a7e7a5c6750a7c0505aedb396dbd2b
Elliott Brest <elliott.brest@gmail.com>
2023-07-25 19:12:52 -0500

    isolated latitude and longitude

--- Assets/script.js
+++ Assets/script.js
@@ -1,45 +1,29 @@
 let cityName = document.querySelector('#userInput');
 let theButton = document.querySelector(".btn");
-// let urlRequest = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=4d8f86000d241e776281dc749be197b9`;
 
 console.log();
 
 theButton.addEventListener('click', GO);
 
 function GO (event) {
-  let locationRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName.value + '&limit=1&appid=4d8f86000d241e776281dc749be197b9';
+const locationRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName.value + '&limit=1&appid=4d8f86000d241e776281dc749be197b9';
   fetch(locationRequest)
     .then(function(response) {
       return response.json();
     })
-    .then(function (BecauseItHasToMatchToFinishTheExpression) {
-      console.log(BecauseItHasToMatchToFinishTheExpression)
-    })
+    .then(function (newResponse) {
+      const latitude = newResponse[0].lat;
+      const longitude = newResponse[0].lon;
+      const urlRequest = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude.value + '&lon=' + longitude.value + '&appid=4d8f86000d241e776281dc749be197b9';
+      console.log(latitude)
+      console.log(longitude)
+      fetch(urlRequest)
+        .then(function(response) {
+          return response.json()
+          .then(function (newResponseAgain) {
+            console.log(newResponseAgain)
+    });
+      })
+      })
     event.preventDefault();
   };
-
-  
-
-
-
-
-
-  // document.location.replace('./search-results.html')
-    
-  // if(response) {
-    //   alert("please enter a valid city name");
-    // } else {
-      
-      //     .then (function (response) {
-        //     if (!response.ok) {
-          //     console.log('for the love of god');
-          //   } else {
-  //     response.json()
-  //       .then (function() {
-  //         currentTarget.preventDefault()
-  //         console.log();
-  //     console.log("You are here?");
-  //     document.location.replace("./index.html");
-  //     });
-  //   }
-  // })
