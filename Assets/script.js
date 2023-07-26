793b6ee5047f3fc76305f05c5e27670db21e5739
Elliott Brest <elliott.brest@gmail.com>
2023-07-26 10:51:07 -0500

    dynamically added current weather info

--- Assets/script.js
+++ Assets/script.js
@@ -6,15 +6,16 @@ let feelsLike = document.querySelector(".feels-like");
 let highTemp = document.querySelector(".high");
 let lowTemp = document.querySelector(".low");
 let cards = document.querySelectorAll(".card");
-let icon = document.querySelector(".icon");
+let test = document.querySelector(".test");
 
 theButton.addEventListener('click', GO);
 
 function GO (event) {
   const locationRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName.value + '&limit=1&appid=4d8f86000d241e776281dc749be197b9';
-historyButtons (cityName);
-console.log(cityName.value);
-fetchInfo(locationRequest);
+  historyButtons (cityName);
+  console.log();
+  fetchInfo(locationRequest);
+  event.preventDefault();
 }  
 function fetchInfo(locationRequest){
   fetch(locationRequest)
@@ -32,28 +33,34 @@ function fetchInfo(locationRequest){
         .then(function(response) {
           return response.json()
         })
-        .then(function (secRes) {
-          console.log(secRes)
-          sharedData (secRes, newCity);
-          // for (i=0; i <= cards.length; i++) {
-          //     icon.textContent = secRes.list[i].weather.icon
-          //     rightNow.textContent = secRes.list[i].main.temp
-          //     weatherCond.textContent = secRes.list[i].weather[0].description
-          //     feelsLike.textContent = ("Feels like: " + secRes.list[i].main.feels_like)
-          //     lowTemp.textContent = ('Low: ' + secRes.list[i].main.temp_min)
-          //     highTemp.textContent = ('High: ' + secRes.list[i].main.temp_max)
-          // }
-    })
-  })
-    event.preventDefault();
-};
-
-function sharedData (secRes, newCity) {
-  // currentWeather (newCity, secRes.list[0]);
-  // forecastWeather (secRes.list);
-  console.log(secRes, newCity);
-}
-
-function historyButtons(city) {
-  console.log(city.value);
+        .then(function (secRequ) {
+          console.log(secRequ)
+          sharedData (secRequ, newCity);
+        })
+      })
+    };
+    
+    function sharedData (secRequ, newCity) {
+      currentWeather (newCity, secRequ.list[0]);
+      // forecastWeather (secRequ.list);
+      console.log();
+    }
+    
+    function historyButtons(city) {
+      console.log(city);
+    }
+    
+    function currentWeather (newCity, data) {
+      let currentCard = document.createElement("p");
+      test.append(currentCard);
+      currentCard.textContent =
+        [
+          data.main.temp,
+          data.weather[0].description,
+          ("Feels like: " + data.main.feels_like),
+          ('Low: ' + data.main.temp_min),
+          ('High: ' + data.main.temp_max),
+        ];      
+      console.log(currentCard);
+      console.log(test);
 }
